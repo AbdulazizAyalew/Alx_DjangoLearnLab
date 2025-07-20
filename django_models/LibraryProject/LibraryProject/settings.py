3907d8f9b14f73a5be40aceceda281cf063b3ec0
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-20 20:56:37 +0300

    Added Login redirect url

--- django_models/LibraryProject/LibraryProject/settings.py
+++ django_models/LibraryProject/LibraryProject/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'  # or any page you want
 
 
 # Quick-start development settings - unsuitable for production
