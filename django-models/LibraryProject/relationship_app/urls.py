46b6ac747fb4138ef3c0e52969129685a51bfe58
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-25 20:52:27 +0300

    chore: Imported the neccessary views for usage in the url configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from views import LibraryDetailView
+from views import LibraryDetailView,list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
