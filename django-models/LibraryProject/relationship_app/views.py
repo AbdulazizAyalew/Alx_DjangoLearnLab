ef2a3ba2a612d7c7725a7a4d4faa1cbb662a68c4
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-25 20:32:49 +0300

    feat: Add view to list books and link it to be rendered on book_list.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request,'relationship_app/book_list.html',{'books':books})
