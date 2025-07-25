1a57ddb0b0a8d65afb8d2246a640399263dd56aa
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-25 14:10:40 +0300

    feat: changed the query for the retrival of books from the Library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,7 +7,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')  # Replace with your project name
 django.setup()
 
-from relationship_app.models import Author, Book, Library
+from relationship_app.models import *
 
 # 1. Query all books by a specific author
 author_name = "John Doe"
@@ -24,7 +24,7 @@ except Author.DoesNotExist:
 library_name = "Central Library"
 try:
     library = Library.objects.get(name=library_name)
-    books_in_library = library.books.all()
+    books_in_library = Librarian.objects.get(library=library)
     print(f"\nBooks in {library.name}:")
     for book in books_in_library:
         print(f"- {book.title} by {book.author.name}")
