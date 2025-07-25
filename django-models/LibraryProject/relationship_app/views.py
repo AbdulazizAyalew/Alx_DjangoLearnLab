e3c588c10838d4cfdc926137026aabeed5d97d7e
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-25 20:47:46 +0300

    fix: fixed template path for list_books view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import *
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request,'relationship_app/book_list.html',{'books':books})
+    return render(request,'relationship_app/list_books.html',{'books':books})
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
