41c49d5dc335158db6efdfaec61e6d70f76f24d7
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-26 11:41:08 +0300

    Split permission_required import for auto-check compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from .models import Library,Book,UserProfile
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 # Create your views here.
 
 def list_books(request):
