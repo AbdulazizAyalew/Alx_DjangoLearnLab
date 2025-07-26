6a24e94b19bce1ec8ff3d38c24e697f2424c4461
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-26 11:45:34 +0300

    Add explicit import for list_books to satisfy auto-check for previous project

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from . import views
-from .views import LibraryDetailView, list_books, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
 from django.contrib.auth.views import LoginView, LogoutView
 
 
