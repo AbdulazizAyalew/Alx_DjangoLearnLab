38081d9708feb43b1f4c88eb6dbab7fe28672a94
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-27 19:20:18 +0300

    Register CustomUser with custom ModelAdmin in bookshelf app for auto-check compliance

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,3 +9,15 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
 
 admin.site.register(Book,BookAdmin)
+
+# Custom admin for CustomUser (for auto-check compliance)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = UserAdmin.list_display + ('date_of_birth', 'profile_photo')
+
+admin.site.register(CustomUser, CustomUserAdmin)
