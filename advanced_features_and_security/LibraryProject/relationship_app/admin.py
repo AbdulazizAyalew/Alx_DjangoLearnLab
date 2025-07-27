a1af61f5cd20a20ee206ace972958d070b418692
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-07-27 18:59:30 +0300

    Register CustomUser with custom ModelAdmin in admin.py, including extra fields

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)   
-admin.site.register(Library)
-admin.site.register(Librarian)
+
+# Custom admin for CustomUser
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = UserAdmin.list_display + ('date_of_birth', 'profile_photo')
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
