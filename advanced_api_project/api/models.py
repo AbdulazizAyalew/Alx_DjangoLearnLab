d7c6da82cdec3daa428415d3bb91413e1da5e30a
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-08-10 21:02:05 +0300

    feat: Add models and migrated them

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)  # Store author name
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)  # Book title
+    publication_year = models.IntegerField()  # Year of publication
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    # One Author can have many Books
+
+    def __str__(self):
+        return self.title
